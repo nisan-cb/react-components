bd2fe0f7ab1d9c1dab7d8460770e6350cf959577
nisan-cb <nnisan858@gmail.com>
2022-07-17 06:02:26 +0300

    calendar component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Calendar from './components/Calendar/Calendar';
 function App() {
   return (
     <div className="App">
-      <Calendar></Calendar>
+      <Calendar />
     </div>
   );
 }
--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,13 +1,55 @@
-import React from "react";
-export default function Calendar() {
+// import React, { useEffect } from "react";
+import './Calendar.scss'
 
 
+export default function Calendar({ year = new Date().getFullYear(), month = new Date().getMonth() }) {
+    const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    // const months = ['January', 'February', 'March', 'April', 'May', 'June',
+    //     'July', 'August', 'September', 'October', 'November', 'December'];
+
+    const today = new Date();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    let daysOfPrevMonth = new Date(year, month).getDay();
+    let daysOfNextMonth = 6 - new Date(year, month + 1, 0).getDay();
+    const days = [];
+    let i = 1 - daysOfPrevMonth;
+    const numOfDays = daysInMonth + daysOfNextMonth;
+    for (; i <= numOfDays; i++)
+        days.push(new Date(year, month, i))
+
+    const rows = [];
+    for (let i = 0; i < days.length; i = i + 7) {
+        const week = days.slice(i, i + 7);
+        const tr = <tr key={i}>
+            {
+                week.map(day => {
+                    return <td key={day.toDateString()} className={'calendar-day' + ((day.getMonth() === month) ? ' this-month ' : '')}>
+                        <p className={day.toDateString() === today.toDateString() ? 'today' : ''}>
+                            {day.getDate()}
+                        </p>
+                    </td>
+                })
+            }
+        </tr>
+        rows.push(tr);
+    }
     return (
-        <>
+        <div id='calendar'>
             <p>Calendar</p>
+            <p>today: {`${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`}</p>
             <table>
+                <tbody>
+                    <tr>
+                        {
+                            weekdays.map(day => <th key={'th' + day}>{day}</th>)
+                        }
+                    </tr>
 
+                    {rows}
+
+                </tbody>
             </table>
-        </>
+        </div>
     )
 }
+
